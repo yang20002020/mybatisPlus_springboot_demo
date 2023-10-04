ce5ab089755bb5a4934ebd4b66b4e9a94a67c63f
yang20002020 <2628636987@qq.com>
2023-10-04 09:49:19 +0800

    性能分析插件

---
+++ mybatisPlus_springboot_09/src/test/java/com/fuyu/MybatisPlusSpringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fuyu;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
